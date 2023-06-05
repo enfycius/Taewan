813c7752e743b9b6202ea00d3bdef4c9a59ccc02
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-06-05 23:06:59 +0900

    23.06.05

---
+++ 6.5/app/src/main/java/com/example/imagedownloader/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.imagedownloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private int REQUEST_INTERNET = 200;
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            response = httpConn.getResponseCode();
+
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+            Log.d("Networking", ex.getLocalizedMessage());
+
+            throw new IOException("Error Connecting");
+        }
+
+        return in;
+    }
+
+    private Bitmap DownloadImage(String URL) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException ex) {
+            Log.d("NetworkingActivity", ex.getLocalizedMessage());
+        }
+
+        return bitmap;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... urls) {
+            return DownloadImage(urls[0]);
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap(result);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.urlEdit);
+
+        Button button = (Button) findViewById(R.id.submit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DownloadImageTask().execute(editText.getText().toString());
+            }
+        });
+
+
+
+
+    }
+}
